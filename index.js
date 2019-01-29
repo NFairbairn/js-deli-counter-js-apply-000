79a4d74decefd95528292fcfaa55abe0468f3416
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-29 18:07:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine, name) {
-
+//removes the first person in line
   var name = katzDeliLine.shift();
 
+//checks if there is anyone in line
 if (katzDeliLine.length > 0) {
   return `Currently serving ${name}.`
 } else {
@@ -18,13 +19,20 @@ if (katzDeliLine.length > 0) {
 
 
 function currentLine(line){
+//checks to see if anyone is in line
    if (line.length === 0) {
      return 'The line is currently empty.';
    } else{
      let emptyArr = [];
+//(establishes a counter, checks if counter is less than the length of the line,
+//incriments counter)
    for (let i = 0; i < line.length; i++) {
+//creates the numbered list with counter +1. who's in line[place in line]
+//then pushes that string into the empty array
      emptyArr.push(`${i + 1}. ${line[i]}`);
    }
+   //.join used to join all string elements together
+   //set .join() to separate by a comma for the list
    return 'The line is currently: ' + emptyArr.join(', ');
    }
  }
